7e9d31ebced00d5f2405541465c053f2c847017b
Yunhong Min <yhmin84@gmail.com>
2019-05-19 15:25:59 +0900

    config added

--- server/main.py
+++ server/main.py
@@ -1,8 +1,21 @@
+import argparse
 
 from aiohttp import web
+import yaml
 
 from routes import setup_routes
 
+def read_config(path):
+    with open(path) as f:
+        config = yaml.safe_load(f)
+    return config
+
+parser = argparse.ArgumentParser()
+parser.add_argument('--config', help='config file path')
+args = parser.parse_args()
+config = read_config(args.config)
+
 app = web.Application()
 setup_routes(app)
+app['config'] = config
 web.run_app(app)

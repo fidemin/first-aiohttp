c0f36935bece936d2fe436dcd6d8cc762e1b65ec
Yunhong Min <yhmin84@gmail.com>
2019-05-19 17:28:38 +0900

    json response

--- server/views.py
+++ server/views.py
@@ -6,6 +6,7 @@ from db import question
 async def index(request):
     async with request.app['db'].acquire() as conn:
         cursor = await conn.execute(question.select())
+        ret = []
         async for row in cursor:
-            print(row)
-    return web.Response(text='Hello aiohttp')
+            ret.append(row.question_text)
+    return web.json_response(ret)
